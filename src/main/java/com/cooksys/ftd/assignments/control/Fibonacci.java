d703e9733f94f30c80ededa846be3d687dd88a1c
therandomfool <mddenver@protonmail.com>
2021-03-03 16:33:16 -0800

    Parts done for fizz and fib

--- src/main/java/com/cooksys/ftd/assignments/control/Fibonacci.java
+++ src/main/java/com/cooksys/ftd/assignments/control/Fibonacci.java
@@ -1,54 +1,102 @@
 package com.cooksys.ftd.assignments.control;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 /**
- * The Fibonacci sequence is simply and recursively defined: the first two elements are `1`, and
- * every other element is equal to the sum of its two preceding elements. For example:
+ * The Fibonacci sequence is simply and recursively defined: the first two
+ * elements are `1`, and every other element is equal to the sum of its two
+ * preceding elements. For example:
  * <p>
- * [1, 1] =>
- * [1, 1, 1 + 1]  => [1, 1, 2] =>
- * [1, 1, 2, 1 + 2] => [1, 1, 2, 3] =>
- * [1, 1, 2, 3, 2 + 3] => [1, 1, 2, 3, 5] =>
- * ...etc
+ * [1, 1] => [1, 1, 1 + 1] => [1, 1, 2] => [1, 1, 2, 1 + 2] => [1, 1, 2, 3] =>
+ * [1, 1, 2, 3, 2 + 3] => [1, 1, 2, 3, 5] => ...etc
  */
 public class Fibonacci {
 
-    /**
-     * Calculates the value in the Fibonacci sequence at a given index. For example,
-     * `atIndex(0)` and `atIndex(1)` should return `1`, because the first two elements of the
-     * sequence are both `1`.
-     *
-     * @param i the index of the element to calculate
-     * @return the calculated element
-     * @throws IllegalArgumentException if the given index is less than zero
-     */
-    public static int atIndex(int i) throws IllegalArgumentException {
-        throw new NotImplementedException();
-    }
+	/**
+	 * Calculates the value in the Fibonacci sequence at a given index. For example,
+	 * `atIndex(0)` and `atIndex(1)` should return `1`, because the first two
+	 * elements of the sequence are both `1`.
+	 *
+	 * @param i the index of the element to calculate
+	 * @return the calculated element
+	 * @throws IllegalArgumentException if the given index is less than zero
+	 */
+	public static int atIndex(int i) throws IllegalArgumentException {
+//    	fib array to arrive at index
+		if (i < 0) {
+			throw new IllegalArgumentException("index is less than zero");	
+		}
+		
+		int temp[] = new int[i+2];
+		temp[0] = 1;
+		temp[1] = 1;
+		
+		for(int j = 2; j <= i; j++) {
+			temp[j] = temp[j-1] + temp[j-2];
+		}
+		
+		return temp[i];
 
-    /**
-     * Calculates a slice of the fibonacci sequence, starting from a given start index (inclusive) and
-     * ending at a given end index (exclusive).
-     *
-     * @param start the starting index of the slice (inclusive)
-     * @param end   the ending index of the slice(exclusive)
-     * @return the calculated slice as an array of int elements
-     * @throws IllegalArgumentException if either the given start or end is negative, or if the
-     *                                  given end is less than the given start
-     */
-    public static int[] slice(int start, int end) throws IllegalArgumentException {
-        throw new NotImplementedException();
-    }
 
-    /**
-     * Calculates the beginning of the fibonacci sequence, up to a given count.
-     *
-     * @param count the number of elements to calculate
-     * @return the beginning of the fibonacci sequence, up to the given count, as an array of int elements
-     * @throws IllegalArgumentException if the given count is negative
-     */
-    public static int[] fibonacci(int count) throws IllegalArgumentException {
-        throw new NotImplementedException();
-    }
+	}
+
+	/**
+	 * Calculates a slice of the fibonacci sequence, starting from a given start
+	 * index (inclusive) and ending at a given end index (exclusive).
+	 *
+	 * @param start the starting index of the slice (inclusive)
+	 * @param end   the ending index of the slice(exclusive)
+	 * @return the calculated slice as an array of int elements
+	 * @throws IllegalArgumentException if either the given start or end is
+	 *                                  negative, or if the given end is less than
+	 *                                  the given start
+	 */
+	public static int[] slice(int start, int end) throws IllegalArgumentException {
+		if (end < start || end < 0 || start < 0) {
+			throw new IllegalArgumentException(
+					"either the given start or end is negative, or if the given end is less than the given start");
+		}
+		
+//		int size = start - end;
+//		int part[] = new int[size];
+//		// Copying the contents of the array
+//		for (int i = 0; i < part.length; i++) {
+//			part[i] = part[start + i];
+//		}
+//		return part;
+		
+		int tempSlice[] = new int[end - start];
+		int arraySlice[]= fibonacci(end);
+		
+		for(int j = 0; j <= tempSlice.length; j++) {
+			tempSlice[j] = arraySlice[start+j];
+		}
+		
+		
+
+		return tempSlice;
+	}
+
+	/**
+	 * Calculates the beginning of the fibonacci sequence, up to a given count.
+	 *
+	 * @param count the number of elements to calculate
+	 * @return the beginning of the fibonacci sequence, up to the given count, as an
+	 *         array of int elements
+	 * @throws IllegalArgumentException if the given count is negative
+	 */
+	public static int[] fibonacci(int count) throws IllegalArgumentException {
+		if (count < 0) {
+			throw new IllegalArgumentException();
+		}
+		
+		
+		
+		
+		
+		if (count <= 1) {
+			int[] temp = new int[count + 1];
+			temp[0] = 1;
+			temp[1] = 1;
+		}
+		return null;
+	}
 }
--- src/main/java/com/cooksys/ftd/assignments/control/FizzBuzz.java
+++ src/main/java/com/cooksys/ftd/assignments/control/FizzBuzz.java
@@ -1,69 +1,99 @@
 package com.cooksys.ftd.assignments.control;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 /**
- * FizzBuzz is an old programming exercise.
- * The goal is to iterate over a range of numbers and print a message about each number's divisibility.
+ * FizzBuzz is an old programming exercise. The goal is to iterate over a range
+ * of numbers and print a message about each number's divisibility.
  * <p>
- * The message is generated the following way:
- * *) if the number is divisible by three, the message is `Fizz`
- * *) if the number is divisible by five, the message is `Buzz`
- * *) if the number is divisible by both three and five, the message is `FizzBuzz`
- * *) otherwise, no message is produced
+ * The message is generated the following way: *) if the number is divisible by
+ * three, the message is `Fizz` *) if the number is divisible by five, the
+ * message is `Buzz` *) if the number is divisible by both three and five, the
+ * message is `FizzBuzz` *) otherwise, no message is produced
  * <p>
- * The exact message format for this assignment is specified in the `message(n)` method.
+ * The exact message format for this assignment is specified in the `message(n)`
+ * method.
  */
 public class FizzBuzz {
 
-    /**
-     * Checks whether a given int `a` is evenly divisible by a given int `b` or not.
-     * For example, `divides(4, 2)` returns `true` and `divides(4, 3)` returns `false`.
-     *
-     * @param a the number to be divided
-     * @param b the number to divide by
-     * @return `true` if a is evenly divisible by b, `false` otherwise
-     * @throws IllegalArgumentException if b is zero
-     */
-    public static boolean divides(int a, int b) throws IllegalArgumentException {
-        throw new NotImplementedException();
-    }
+	/**
+	 * Checks whether a given int `a` is evenly divisible by a given int `b` or not.
+	 * For example, `divides(4, 2)` returns `true` and `divides(4, 3)` returns
+	 * `false`.
+	 *
+	 * @param a the number to be divided
+	 * @param b the number to divide by
+	 * @return `true` if a is evenly divisible by b, `false` otherwise
+	 * @throws IllegalArgumentException if b is zero
+	 */
+	public static boolean divides(int a, int b) throws IllegalArgumentException {
+		if (b == 0) {
+			throw new IllegalArgumentException("b can not be zero");
+		}
+		return (a % b == 0);
 
-    /**
-     * Generates a divisibility message for a given number. Returns null if the given number is not divisible by 3 or 5.
-     * Message formatting examples:
-     * 1 -> null // not divisible by 3 or 5
-     * 3 -> "3: Fizz" // divisible by only 3
-     * 5 -> "5: Buzz" // divisible by only 5
-     * 15 -> "15: FizzBuzz" // divisible by both three and five
-     *
-     * @param n the number to generate a message for
-     * @return a message according to the format above, or null if n is not divisible by either 3 or 5
-     */
-    public static String message(int n) {
-        throw new NotImplementedException();
-    }
+	}
 
-    /**
-     * Generates an array of messages to print for a given range of numbers.
-     * If there is no message for an individual number (i.e., `message(n)` returns null),
-     * it should be excluded from the resulting array.
-     *
-     * @param start the number to start with (inclusive)
-     * @param end the number to end with (exclusive)
-     * @return an array of divisibility messages
-     * @throws IllegalArgumentException if the given end is less than the given start
-     */
-    public static String[] messages(int start, int end) throws IllegalArgumentException {
-        throw new NotImplementedException();
-    }
+	/**
+	 * Generates a divisibility message for a given number. Returns null if the
+	 * given number is not divisible by 3 or 5. Message formatting examples: 1 ->
+	 * null // not divisible by 3 or 5 3 -> "3: Fizz" // divisible by only 3 5 ->
+	 * "5: Buzz" // divisible by only 5 15 -> "15: FizzBuzz" // divisible by both
+	 * three and five
+	 *
+	 * @param n the number to generate a message for
+	 * @return a message according to the format above, or null if n is not
+	 *         divisible by either 3 or 5
+	 */
+	public static String message(int n) {
+		if (n % 3 == 0 && n % 5 == 0) {
+			return (n + ": FizzBuzz");
+		} else if (n % 5 == 0 ) {
+			return (n + ": Buzz");
+		} else if (n % 3 == 0 ) {
+			return (n + ": Fizz");
+		} else {
+			return null;
+		}
 
-    /**
-     * For this main method, iterate over the numbers 1 through 115 and print
-     * the relevant messages to sysout
-     */
-    public static void main(String[] args) {
-        throw new NotImplementedException();
-    }
+	}
+
+	/**
+	 * Generates an array of messages to print for a given range of numbers. If
+	 * there is no message for an individual number (i.e., `message(n)` returns
+	 * null), it should be excluded from the resulting array.
+	 *
+	 * @param start the number to start with (inclusive)
+	 * @param end   the number to end with (exclusive)
+	 * @return an array of divisibility messages
+	 * @throws IllegalArgumentException if the given end is less than the given
+	 *                                  start
+	 */
+	public static String[] messages(int start, int end) throws IllegalArgumentException {
+		if (end < start) {
+			throw new IllegalArgumentException("The end can not be less than the start");
+		}
+
+		String[] divArray = new String[end - start];
+
+		for (int i = start; i < divArray.length; i++) {
+
+		}
+
+		return null;
+	}
+
+	/**
+	 * For this main method, iterate over the numbers 1 through 115 and print the
+	 * relevant messages to sysout
+	 */
+	public static void main(String[] args) {
+		
+		
+		for(int i = 1; i <= 115; i++) {
+			
+//			System.out.println(messages(1, 115));
+		}
+		
+		return;
+	}
 
 }
